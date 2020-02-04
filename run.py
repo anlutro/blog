b755f94202bb4c0027d19f60b39012cf9dc33159
Andreas Lutro <anlutro@gmail.com>
2020-02-04 13:35:59 +0100

    black

--- run.py
+++ run.py
@@ -9,55 +9,59 @@ from russell.engine import BlogEngine
 
 ROOT_DIR = os.path.dirname(__file__)
 
+
 def validate_sass_file(sass_file):
-	uses_tabs = None
-	with open(sass_file) as file:
-		line_no = 0
-		for line in file:
-			line_no += 1
-			if uses_tabs is None:
-				if line.startswith('  '):
-					uses_tabs = False
-				elif line.startswith('\t'):
-					uses_tabs = True
-			elif uses_tabs is True and '  ' in line:
-				print('%s uses tabs but spaces found on line #%d' % (sass_file, line_no))
-			elif uses_tabs is False and '\t' in line:
-				print('%s uses spaces but tab found on line #%d' % (sass_file, line_no))
+    uses_tabs = None
+    with open(sass_file) as file:
+        line_no = 0
+        for line in file:
+            line_no += 1
+            if uses_tabs is None:
+                if line.startswith("  "):
+                    uses_tabs = False
+                elif line.startswith("\t"):
+                    uses_tabs = True
+            elif uses_tabs is True and "  " in line:
+                print(
+                    "%s uses tabs but spaces found on line #%d" % (sass_file, line_no)
+                )
+            elif uses_tabs is False and "\t" in line:
+                print("%s uses spaces but tab found on line #%d" % (sass_file, line_no))
+
 
 def generate_css():
-	sass_dir = os.path.join(ROOT_DIR, 'sass')
-	sass_files = [
-		f.path for f in os.scandir(sass_dir)
-		if f.path.endswith('.sass')
-	]
-	for sass_file in sass_files:
-		validate_sass_file(sass_file)
-	sass_main = os.path.join(sass_dir, 'main.sass')
-	return sass.compile(filename=sass_main)
+    sass_dir = os.path.join(ROOT_DIR, "sass")
+    sass_files = [f.path for f in os.scandir(sass_dir) if f.path.endswith(".sass")]
+    for sass_file in sass_files:
+        validate_sass_file(sass_file)
+    sass_main = os.path.join(sass_dir, "main.sass")
+    return sass.compile(filename=sass_main)
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root-url', default='//www.lutro.me')
-parser.add_argument('-v', '--verbose', action='store_true')
+parser.add_argument("--root-url", default="//www.lutro.me")
+parser.add_argument("-v", "--verbose", action="store_true")
 args = parser.parse_args()
 
 logging.basicConfig(
-	level=logging.DEBUG if args.verbose else logging.INFO,
-	format='%(asctime)s [%(levelname)8s] [%(name)s] %(message)s',
+    level=logging.DEBUG if args.verbose else logging.INFO,
+    format="%(asctime)s [%(levelname)8s] [%(name)s] %(message)s",
 )
 
 blog = BlogEngine(
-	ROOT_DIR,
-	root_url=args.root_url,
-	site_title='lutro.me',
-	site_desc=("Andreas Lutro's personal website/blog. "
-	           "Mostly programming and Linux sysadmin stuff.")
+    ROOT_DIR,
+    root_url=args.root_url,
+    site_title="lutro.me",
+    site_desc=(
+        "Andreas Lutro's personal website/blog. "
+        "Mostly programming and Linux sysadmin stuff."
+    ),
 )
 blog.add_pages()
 blog.add_posts()
 
 blog.copy_assets()
-blog.write_file('assets/style.css', generate_css())
+blog.write_file("assets/style.css", generate_css())
 blog.add_asset_hashes()
 
 blog.generate_index(num_posts=4)
@@ -71,4 +75,4 @@ blog.generate_tags()
 blog.generate_sitemap(https=True)
 
 blog.generate_rss(https=True, only_excerpt=False)
-blog.write_file('robots.txt', 'User-agent: *\nDisallow:\n')
+blog.write_file("robots.txt", "User-agent: *\nDisallow:\n")
--- serve.py
+++ serve.py
@@ -6,29 +6,30 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
 class RequestHandler(SimpleHTTPRequestHandler):
-	def translate_path(self, path):
-		path = super().translate_path(path)
-		try_paths = (path, path + '.html', path + '/index.html')
-		for try_path in try_paths:
-			if os.path.exists(try_path):
-				return try_path
-		return path
+    def translate_path(self, path):
+        path = super().translate_path(path)
+        try_paths = (path, path + ".html", path + "/index.html")
+        for try_path in try_paths:
+            if os.path.exists(try_path):
+                return try_path
+        return path
 
 
 def run_server(root_dir=None):
-	if root_dir:
-		os.chdir(root_dir)
+    if root_dir:
+        os.chdir(root_dir)
 
-	server_address = ('127.0.0.1', 8000)
+    server_address = ("127.0.0.1", 8000)
 
-	httpd = HTTPServer(server_address, RequestHandler)
-	sa = httpd.socket.getsockname()
-	print('Serving HTTP on http://%s:%s/ ...' % sa)
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		pass
-	finally:
-		httpd.server_close()
+    httpd = HTTPServer(server_address, RequestHandler)
+    sa = httpd.socket.getsockname()
+    print("Serving HTTP on http://%s:%s/ ..." % sa)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        httpd.server_close()
 
-run_server(os.path.join(os.path.dirname(__file__), 'dist'))
+
+run_server(os.path.join(os.path.dirname(__file__), "dist"))
